9d75749ae988132ec83ec33e46f4425a15f812c1
zephyr <jafaribrownsha@gmail.com>
2023-10-23 17:13:24 -0400

    helpful hint added

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/',(req,res) => {
     res.send('Hello World')
 })
 
+
+//Wild card page make always ensure it ends up on the bottom.
 app.get('*',(req,res) =>{
     res.status.send('<h1>404 Page</h1>')
 })
