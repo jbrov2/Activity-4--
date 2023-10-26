561c4cebbd7fb3b4da072cdeff171eca08338149
zephyr <jafaribrownsha@gmail.com>
2023-10-25 20:38:04 -0400

    added places

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,27 @@
 const router = require('express').Router()
 
+
 router.get('/', (req,res) =>{
-    res.send('GET /places')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Kansas Fried Donuts',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Donuts, Chicken',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', {places})
 })
+
+// //GET ?Palces
+// app.get('/', (req, res)=>{
+//     res.render('places/index')
+// })
 
 module.exports = router
