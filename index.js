fc0c652676b763dae66435c471540dda0e4c4f26
zephyr <jafaribrownsha@gmail.com>
2023-10-28 12:17:42 -0400

    rearranging code

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
+//MIDDLEWARE SECTION
 //adding jsx
 app.set('views',__dirname+'/views')
 app.set('view engine','jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+//adding body parser
+app.use(express.urlencoded({extended:true}))
+//MIDDLEWARE SECTION
+
+
 
 //adding controller
 app.use('/places', require('./controllers/places'))
+
 
 app.get('/',(req,res) => {
     res.render('home')
