e3ffd3141950f8a8ca63c25296dfd2536b29a138
zephyr <jafaribrownsha@gmail.com>
2023-10-28 12:16:46 -0400

    gave people the ability to add new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,35 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
 
 
 router.get('/', (req,res) =>{
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/gurpartap-sandhu-nuAOmcjs3ZQ-unsplash.jpg'
-      }, {
-        name: 'Kansas Fried Donuts',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Donuts, Chicken',
-        pic: '/images/goku.png'
-      }]
       
     res.render('places/index', {places})
 })
+
+router.get('/new', (req,res)=>{
+  res.render('places/new')
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+
 
 // //GET ?Palces
 // app.get('/', (req, res)=>{
