0f3a9c0af865bcd505fb0a962e27dfa5630a1782
zephyr <jafaribrownsha@gmail.com>
2023-10-28 13:07:12 -0400

    added css

--- views/default.jsx
+++ views/default.jsx
@@ -8,6 +8,19 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
             </head>
             <body>
+                <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                            <a className="navbar-brand" href="/">Home</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className="nav-link" href="/places">Places</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className="nav-link" href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
