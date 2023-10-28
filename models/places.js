02c85288c527ce46a8e18be4b2b879a24b66a910
zephyr <jafaribrownsha@gmail.com>
2023-10-28 12:16:59 -0400

    the ability to add palces

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/gurpartap-sandhu-nuAOmcjs3ZQ-unsplash.jpg'
+  }, {
+    name: 'Kansas Fried Donuts',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Donuts, Chicken',
+    pic: '/images/goku.png'
+  }]
